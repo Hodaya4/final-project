ebe4c8c6e0376a64722449d5b7f30f7bed3672bf
osnatamar <osnat1707@gmail.com>
2024-03-17 10:48:16 +0200

    Merge remote-tracking branch 'origin/master'

--- components/LoginScreen.js
+++ components/LoginScreen.js
@@ -1,27 +1,51 @@
-// LoginScreen.js
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native';
+//שאלון שעל בסיסו אפשר לדעת צריך לחבר api
+const LoginScreen = () => {
+    const [responses, setResponses] = useState({});
 
-import React from 'react';
-import {Keyboard, StyleSheet, Text, TextInput, TouchableOpacity, TouchableWithoutFeedback, View} from 'react-native';
+    const handleResponseChange = (question, response) => {
+        setResponses(prevResponses => ({
+            ...prevResponses,
+            [question]: response,
+        }));
+    };
 
-
-export default class LoginScreen extends React.Component {
-    state = {
+    const handleSubmit = () => {
+        console.log('Responses:', responses);
 
     };
 
-    handleLogin = () => {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Familiarization  Questionnaire</Text>
+            <Text style={styles.question}>What genres do you enjoy reading?</Text>
+            <TextInput
+                style={styles.input}
+                onChangeText={response => handleResponseChange('genres', response)}
+                placeholderTextColor="#003f5c"
+            />
 
-    };
+            <Text style={styles.question}>Do you prefer short or long books?</Text>
+            <TextInput
+                style={styles.input}
+                onChangeText={response => handleResponseChange('bookLength', response)}
+                placeholderTextColor="#003f5c"
+            />
 
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text style={styles.logo}>Login</Text>
+            <Text style={styles.question}>What mood are you usually in when reading?</Text>
+            <TextInput
+                style={styles.input}
+                onChangeText={response => handleResponseChange('mood', response)}
+                placeholderTextColor="#003f5c"
+            />
 
-            </View>
-        );
-    }
-}
+            <TouchableOpacity style={styles.button} onPress={handleSubmit}>
+                <Text style={styles.buttonText}>Send</Text>
+            </TouchableOpacity>
+        </View>
+    );
+};
 
 const styles = StyleSheet.create({
     container: {
@@ -30,12 +54,43 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         backgroundColor: '#003f5c',
     },
-    logo: {
+    title: {
         fontSize: 24,
         fontWeight: 'bold',
+        color: '#fb5b5a',
         marginBottom: 20,
+    },
+    question: {
+        fontSize: 16,
+        marginBottom: 10,
         color: '#fb5b5a',
     },
+    input: {
+        width: '80%',
+        height: 40,
+        borderWidth: 1,
+        borderColor: '#ccc',
+        borderRadius: 20,
+        marginBottom: 20,
+        paddingHorizontal: 10,
+        backgroundColor: '#465881',
+        color: 'white',
+    },
+    button: {
+         width: '50%',
+         height: 50,
+         backgroundColor: '#fb5b5a',
+         borderRadius: 25,
+         justifyContent: 'center',
+         alignItems: 'center',
+    },
+    buttonText: {
+        fontSize: 17,
+        color: 'black',
+        fontWeight: 'bold',
 
-
+    },
 });
+
+export default LoginScreen;
+
